06b6b55c65a50e39f030a56099bac1f42fdc9bee
Rajat Bhopte <rajatbhopte14@gmail.com>
2026-02-06 16:10:52 +0530

    update cors

--- stock-news-analyzer-backend/src/app.js
+++ stock-news-analyzer-backend/src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import sentimentRoutes from './routes/sentiment.routes.js';
 import newsRoutes from './routes/news.routes.js';
+import cors from "cors"; // Import CORS middleware
 
 const app = express();
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
 
 app.get('/', (req, res) => {
   res.send('Stock News Analyzer Backend is running');
